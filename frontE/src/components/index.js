9cb02910f660d8fb007bdf5f930b6c4788aabc51
supanat00 <supanat90@gmail.com>
2023-05-25 14:28:25 +0700

    first commit

---
+++ frontE/src/components/index.js
@@ -0,0 +1,12 @@
+import Card from './Card';
+import FormField from './FormField';
+import Loader from './Loader';
+import PromptFormField from './PromptFormField';
+
+
+export {
+  Card,
+  FormField,
+  PromptFormField,
+  Loader,  
+};
---
+++ frontE/src/page/index.js
@@ -0,0 +1,11 @@
+import Home from './Home';
+import CreatePost from './CreatePost';
+import TranTexts from './TranTexts';
+import TextsInput from './TextsInput';
+
+export {
+  Home,
+  CreatePost,
+  TranTexts,
+  TextsInput,
+};
