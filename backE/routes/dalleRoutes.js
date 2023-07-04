da6cfe756b94385a5adc34df337d4eb41516c41d
supanat00 <supanat90@gmail.com>
2023-07-04 13:52:25 +0700

    Update editor first

--- backE/routes/dalleRoutes.js
+++ backE/routes/dalleRoutes.js
@@ -16,6 +16,10 @@ router.route('/').get((req, res) => {
   res.status(200).json({ message: 'Hello from DALL-E!' });
 });
 
+router.route('/editor').get((req, res) => {
+  res.status(200).json({ message: 'Hello Editor from DALL-E!' });
+});
+
 router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
@@ -34,5 +38,29 @@ router.route('/').post(async (req, res) => {
     res.status(500).send(error?.response.data.error.message || 'Something went wrong');
   }
 });
+
+router.route('/editor').post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const Response = await openai.createImageEdit({      
+      image,
+      mask,
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json',
+    });
+
+    const image = Response.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+  }
+});
+
+
+
 
 export default router;
